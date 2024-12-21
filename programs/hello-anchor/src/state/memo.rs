4d42ee660191863a4cddf7e32e5c1ed20e04ab54
Gyujeong Hwang <octave08b@gmail.com>
2024-12-21 19:35:52 +0900

    Add comment to clarify usage of InitSpace in Memo struct

--- programs/hello-anchor/src/state/memo.rs
+++ programs/hello-anchor/src/state/memo.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 
 #[account]
-#[derive(InitSpace)]
+#[derive(InitSpace)] // automatically calculate the space required for the struct
 pub struct Memo {
   pub data: u64,
 }
